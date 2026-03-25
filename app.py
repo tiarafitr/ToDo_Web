ad6911d626e1772759cb669d00d317a3d591baee
tiarafitr <tiara.fitriannisha25@gmail.com>
2026-03-25 23:56:14 +0700

    fix_for_deploy

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def delete(id):
         save_tasks(tasks)
     return redirect("/")
 
+import os
+
 if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
